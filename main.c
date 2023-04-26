5cbdaca8b92f51ae55bd0f59f67cba9ac5e91a74
salmalah <s.salmalahmidi@gmail.com>
2023-04-26 06:52:56 -0700

    update

--- main.c
+++ main.c
@@ -11,6 +11,12 @@ int main(int argc, char **argv, char **env)
 	if (argc == 1)
 	{
 		shell_prompt(argv, env);
+		if (_strcmp(buffer, "exit\n") == 0)
+			exit_shell();
+		if (_strcmp(buffer, "env\n") == 0)
+		{
+			get_env(ev);
+		}
 	}
 	return (0);
 }
