56230aa70753ba88d1eca70dc9e724cbf0b63ad8
salmalah <s.salmalahmidi@gmail.com>
2023-04-26 13:29:19 -0700

    test

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(__attribute__((unused))int argc, char **av, char **env)
 			cmd[byte_read - 1] = '\0';
 		if (_strcmp(cmd, "exit\n") == 0)
 			exit_shell();
-		if (_strcmp(cmd, "env\n") == 0)
+		if (_strcmp(cmd, "env") == 0)
 			get_env(env);
 		pid_fork(cmd);
 	}
