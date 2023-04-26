3ca10bd2e0bab552678298483a30335e89cda23c
salmalah <s.salmalahmidi@gmail.com>
2023-04-26 07:30:51 -0700

    test

--- main.c
+++ main.c
@@ -8,17 +8,16 @@
  */
 int main(__attribute__((unused))int argc, char **av, char **env)
 {
-	size_t buffer_size = 0;
 	char  *buffer = NULL;
+	size_t buffer_size = 0;
 	ssize_t byte_read;
-	pid_t pid;
-	int status;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			_prompt("$ ");
 		byte_read = getline(&buffer, &buffer_size, stdin);
+		(void)av;
 		if (byte_read == -1)
 			break;
 		if (_strcmp(buffer, "exit\n") == 0)
@@ -27,23 +26,6 @@ int main(__attribute__((unused))int argc, char **av, char **env)
 		{
 			get_env(env);
 		}
-		buffer[byte_read - 1] = '\0';
-		av[0] = buffer;
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(1);
-		}
-		else if (pid == 0)
-		{
-			if (execve(av[0], av, NULL) == -1)
-				perror(".hsh/");
-			exit(0);
-		}
-		else
-			wait(&status);
-
 	}
 	free(buffer);
 	return (0);
