5b28fbfd05be16b139e24f078df0a83e8f14bafe
salmalah <s.salmalahmidi@gmail.com>
2023-04-26 16:02:52 -0700

    test

--- main.h
+++ main.h
@@ -1,27 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef __MAIN_H__
+#define __MAIN_H__
+/* declaration de bibliotheque */
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-
-
-char **tokenize(char *line);
-void execute_cmd(char **av);
-void print_env(void);
-int _strlen(char *s);
-int _strcmp(char *value1, char *value2);
-char *_strallocat(char *st);
-char *_strcpy(char *dt, char *s);
-char *_strcat(char *dest, char *src);
-int _atoi(char *s);
+/* declaration of prototype */
 int _putchar(char c);
 void _puts(char *str);
-int __exit(char **t);
-char *_path(char *comamd);
+char *_prompt(char *prompt_symbol);
+void execute_command(char **argv);
+int _strcmp(char *value1, char *value2);
 int get_env(char **env);
+void shell_prompt(char **ar, char **ev);
+void pid_fork(char *buffer);
 #endif
