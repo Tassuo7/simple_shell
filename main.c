3805ba5bb3036f0dbc4d50e774e0c8c367720fbd
salmalah <s.salmalahmidi@gmail.com>
2023-04-26 17:59:05 -0700

    exit

--- main.c
+++ main.c
@@ -8,23 +8,39 @@
  */
 int main(__attribute__((unused))int argc, char **av, char **env)
 {
-	char  *cmd = NULL;
+	char  *cmd = NULL, *avrg[] = {NULL, NULL};
 	size_t buffer_size = 0;
 	ssize_t byte_read;
+	pid_t pid;
+	int status;
+	(void)av;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			_prompt("$ ");
 		byte_read = getline(&cmd, &buffer_size, stdin);
-		(void)av;
 		if (byte_read == -1)
 			break;
-		if (_strcmp(av[0], "exit\n") == 0)
+		avrg[0] = cmd;
+		if (_strcmp(cmd, "exit\n") == 0)
 			exit_shell();
 		if (_strcmp(cmd, "env\n") == 0)
 			get_env(env);
-		pid_fork(cmd);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("ERROr");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			if (execve(cmd, avrg, NULL) == -1)
+				perror("./hsh");
+			exit(0);
+		}
+		else
+			wait(&status);
 	}
 	free(cmd);
 	return (0);
