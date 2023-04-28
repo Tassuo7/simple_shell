42f386fbb38befa4123d17219f002daaaf67f352
salmalah <s.salmalahmidi@gmail.com>
2023-04-28 16:38:09 -0700

    test

---
+++ str.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _boucle - a fucntion repeat command
+ * @ar: array of string
+ * @c: string input
+ * Return: nothing
+ */
+void _boucle(char **ar, char *c)
+{
+        int index;
+
+        index = 0;
+	ar[index] = strtok(c, " ");
+	while (c[index])
+        {
+		ar[++index] = strtok(NULL, " ");
+        }
+	ar[index] = NULL;
+}
